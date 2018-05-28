84c0deb9e99eb9c547ab5d0d7dace65a6115bd06
Anthony Piquet <anthony.piquet2@gmail.com>
2018-05-27 20:55:38 -0700

    Adding converter program

--- XML_HTML_Converter/XML_HTML_Converter/Program.cs
+++ XML_HTML_Converter/XML_HTML_Converter/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Xsl;
 
 namespace XML_HTML_Converter
 {
@@ -10,6 +12,12 @@ namespace XML_HTML_Converter
     {
         static void Main(string[] args)
         {
+            XmlUrlResolver resolver = new XmlUrlResolver();
+            resolver.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            // transform xml file to HTML file
+            XslTransform transform = new XslTransform();
+            transform.Load(@"..\..\XLST_Transformation.xslt", resolver);
+            transform.Transform(@"..\..\XML_Report.xml", @"..\HTML_Report.html", resolver);
         }
     }
 }
